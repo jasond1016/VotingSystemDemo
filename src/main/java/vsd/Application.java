a48c6f381e5917df8f64274d51025cceab536264
JasonW <jasond1016@gmail.com>
2020-08-15 12:57:22 +0800

    build(Spring Boot): 导入 Spring Boot，配置数据库连接

---
+++ src/main/java/vsd/Application.java
@@ -0,0 +1,17 @@
+package vsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JasonW
+ * @date 2020/8/15
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
